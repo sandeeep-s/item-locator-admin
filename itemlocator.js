7d6e2d9e9e0774203ae04577fd89849720cb0c4e
Sandeep <urssandy84@gmail.com>
2016-10-14 19:51:00 +0530

    First successfull completion of edit fuunctionality

--- itemlocator.js
+++ itemlocator.js
@@ -12,37 +12,63 @@ app.config(function($routeProvider) {
 
 app.controller("itemController", function($scope, $http, $location) {
 
+	$scope.item = {};
+
 	$http.get('http://localhost:8090/items').then(function(response) {
 		$scope.items = response.data._embedded.items;
 	})
 
-	$scope.saveItem = function() {
+	$scope.saveItem = function(cmd, url) {
 
-		$http({
-			method : "POST",
-			url : "http://localhost:8090/items",
-			data : {
-				name : $scope.iName,
-				code : $scope.iCode
-			}
-		});
-		
+		var dataObject = {
+			name : $scope.item.name,
+			code : $scope.item.code
+		};
+
+		if (cmd == "add") {
+			$http({
+				method : "POST",
+				url : "http://localhost:8090/items",
+				data : dataObject
+			});
+		} else {
+			$http({
+				method : "PUT",
+				url : url,
+				data : dataObject
+			});
+		}
+
 		$location.path("/");
 	};
-	
-	$scope.deleteItem = function(url){
-		
-		alert(url);
-		
+
+	$scope.deleteItem = function(url) {
+
 		$http({
 			method : "DELETE",
 			url : url
 		});
-		
+
 	}
-	
-	$scope.go = function ( path ) {
-		  $location.path( path );
+
+	$scope.openEditForm = function(item) {
+
+		$scope.item.name = item.name;
+		$scope.item.code = item.code;
+		$scope.iUrl = item._links.self.href;
+		$scope.cmd = "edit";
+
+		$location.path("/create-item");
+	}
+
+	$scope.openAddForm = function() {
+
+		$scope.cmd = "add";
+		$location.path("/create-item");
+	}
+
+	$scope.go = function(path) {
+		$location.path(path);
 	};
 
 });
